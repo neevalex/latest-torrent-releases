b1db29350a8e04752969ce2de74bae9bcde74a1f
neevalex <neevalex@gmail.com>
2019-06-30 18:20:57 +0300

    lint

--- html/js/plugins.js
+++ html/js/plugins.js
@@ -1,30 +1,39 @@
+jQuery( document ).ready(
+	function ($) {
 
-jQuery(document).ready(function ($) {
-    
-    function sort() {
-        var $wrapper = $('.list-group');
+		function sort() {
+			var $wrapper = $( '.list-group' );
 
-        $wrapper.find('.list-group-item').sort(function (a, b) {
-        return +b.dataset.order - +a.dataset.order;
-        }).appendTo($wrapper);
-    }
+			$wrapper.find( '.list-group-item' ).sort(
+				function (a, b) {
+					return +b.dataset.order - +a.dataset.order;
+				}
+			).appendTo( $wrapper );
+		}
 
-    function update() {
+		function update() {
 
-        for (let i = 1; i < 4; i++) {
-           $.getJSON('/api/' + i, function (data) {
-                $('.loading').hide();
-                $.each(data, function (index, element) {
-                       // console.log(element);
-                        $('.list-group').append('<a data-order="' + element.popularity + '" target="_blank" href="http://www.rutor.info/' + element.link + '"" class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">' + element.title + '</h5><small class="text-muted">' + element.popularity + '</small></div></a>');
-                        var div = "#" + element.job_id; 
-                });
-               sort();
-            });      
-        }
-        return;
-    }
+			for (let i = 1; i < 4; i++) {
+				$.getJSON(
+					'/api/' + i,
+					function (data) {
+						$( '.loading' ).hide();
+						$.each(
+							data,
+							function (index, element) {
+								// console.log(element);
+								$( '.list-group' ).append( '<a data-order="' + element.popularity + '" target="_blank" href="http://www.rutor.info/' + element.link + '"" class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">' + element.title + '</h5><small class="text-muted">' + element.popularity + '</small></div></a>' );
+								var div = "#" + element.job_id;
+							}
+						);
+						sort();
+					}
+				);
+			}
+			return;
+		}
 
-    update();
+		update();
 
-});
+	}
+);
--- rutor.js
+++ rutor.js
@@ -1,13 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require( 'express' )
+const app     = express()
 
-var request = require('request-promise');
-const cheerio = require('cheerio');
+var request   = require( 'request-promise' );
+const cheerio = require( 'cheerio' );
 var html;
 
-
 var get_html = async function (i, res) {
-	var url = "http://www.rutor.info/browse/" + i + "/0/0/0";
+	var url  = "http://www.rutor.info/browse/" + i + "/0/0/0";
 	var html = [];
 	var data = [];
 
@@ -16,41 +15,53 @@ var get_html = async function (i, res) {
 		uri: url
 	}
 
-	await request(options)
-		.then(async function (response) {
-			var $ = cheerio.load(response);
-			$('.gai,.tum').each(function (i, element) {
-				title = $(this).find('a:nth-child(3)').text();
-				link = $(this).find('a:nth-child(3)').attr('href');
-				popularity = $(this).find('td span.green').text();
-				data.push({
-					title: title,
-					link: link,
-					popularity: parseInt(popularity)
-				});
-			});
-			return (data);
-		});
+	await request( options )
+		.then(
+			async function (response) {
+				var $ = cheerio.load( response );
+				$( '.gai,.tum' ).each(
+					function (i, element) {
+						title      = $( this ).find( 'a:nth-child(3)' ).text();
+						link       = $( this ).find( 'a:nth-child(3)' ).attr( 'href' );
+						popularity = $( this ).find( 'td span.green' ).text();
+						data.push(
+							{
+								title: title,
+								link: link,
+								popularity: parseInt( popularity )
+							}
+						);
+					}
+				);
+				return (data);
+			}
+		);
 
 	return (data);
 };
 
 
-app.use(express.static('html'))
-app.get('/api/:page', (req, res) => {
-	//console.log(req.params.page);
-	get_html(req.params.page, res).then(function (result) {
-			//console.log(result.length);
-		res.send(result);
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
-
-})
+app.use( express.static( 'html' ) )
+app.get(
+	'/api/:page',
+	(req, res) => {
+		//console.log(req.params.page);
+		get_html( req.params.page, res ).then(
+			function (result) {
+				//console.log(result.length);
+				res.send( result );
+			}
+		)
+		.catch(
+			function (error) {
+				console.log( error );
+			}
+		);
+	}
+)
 
 let port = process.env.PORT;
 if (port == null || port == "") {
 	port = 3001;
 }
-app.listen(port);
+app.listen( port );
